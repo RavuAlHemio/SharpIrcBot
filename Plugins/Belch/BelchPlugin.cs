79aba08259986965d0ec7fe4859bfdf2fcea68cc
Ondřej Hošek <hosek@edvlab.tuwien.ac.at>
2015-03-11 09:50:47 +0100

    Belch: implement skittles

--- Plugins/Belch/BelchPlugin.cs
+++ Plugins/Belch/BelchPlugin.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Linq;
 using System.Reflection;
+using System.Text;
 using log4net;
 using Meebey.SmartIrc4net;
 using Newtonsoft.Json.Linq;
@@ -9,6 +11,8 @@ namespace Belch
     public class BelchPlugin : SharpIrcBot.IPlugin
     {
         private static readonly ILog Logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        private static readonly int[] SkittlesCodes = {1, 2, 3, 4, 5, 6, 7, 10, 12, 13};
 
         protected IrcClient Client;
 
@@ -36,6 +40,21 @@ namespace Belch
             if (msgArr.Length == 1 && string.Equals(msgArr[0], "!belch"))
             {
                 Client.SendMessage(SendType.Action, args.Data.Channel, "belches loudly");
+            }
+
+            if (msgArr.Length > 1 && msgArr[0] == "!skittles")
+            {
+                var message = string.Join(" ", msgArr.Skip(1));
+                var skittledMessage = new StringBuilder();
+                for (int i = 0; i < message.Length; ++i)
+                {
+                    int colorCode = SkittlesCodes[i%SkittlesCodes.Length];
+                    skittledMessage.AppendFormat("\x03{0:D2},99{1}", colorCode, message[i]);
+                }
+                // reset formatting
+                skittledMessage.Append("\x0F");
+
+                Client.SendMessage(SendType.Message, args.Data.Channel, skittledMessage.ToString());
             }
         }
     }
