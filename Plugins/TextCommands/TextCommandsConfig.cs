f7700c32c00b4edff68416ad0d77856d36fc3187
Ondřej Hošek <hosek@edvlab.tuwien.ac.at>
2015-09-23 16:22:33 +0200

    add TextCommands plugin

---
+++ Plugins/TextCommands/TextCommandsConfig.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace TextCommands
+{
+    [JsonObject(MemberSerialization.OptOut)]
+    public class TextCommandsConfig
+    {
+        public Dictionary<string, string> CommandsResponses { get; set; }
+
+        public TextCommandsConfig(JObject obj)
+        {
+            CommandsResponses = new Dictionary<string, string>();
+
+            var ser = new JsonSerializer();
+            ser.Populate(obj.CreateReader(), this);
+        }
+    }
+}
---
+++ Plugins/TextCommands/TextCommandsPlugin.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Reflection;
+using log4net;
+using Meebey.SmartIrc4net;
+using Newtonsoft.Json.Linq;
+using SharpIrcBot;
+
+namespace TextCommands
+{
+    public class TextCommandsPlugin : IPlugin
+    {
+        private static readonly ILog Logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        protected readonly TextCommandsConfig Config;
+        protected readonly ConnectionManager ConnectionManager;
+
+        public TextCommandsPlugin(ConnectionManager connMgr, JObject config)
+        {
+            ConnectionManager = connMgr;
+            Config = new TextCommandsConfig(config);
+
+            ConnectionManager.ChannelMessage += HandleChannelMessage;
+            ConnectionManager.QueryMessage += HandlePrivateMessage;
+        }
+
+        private void HandleChannelMessage(object sender, IrcEventArgs args, MessageFlags flags)
+        {
+            try
+            {
+                ActuallyHandleMessage(
+                    message => ConnectionManager.SendChannelMessage(args.Data.Channel, message),
+                    args,
+                    flags
+                );
+            }
+            catch (Exception exc)
+            {
+                Logger.Error("error handling message", exc);
+            }
+        }
+
+        private void HandlePrivateMessage(object sender, IrcEventArgs args, MessageFlags flags)
+        {
+            try
+            {
+                ActuallyHandleMessage(
+                    message => ConnectionManager.SendQueryMessage(args.Data.Nick, message),
+                    args,
+                    flags
+                );
+            }
+            catch (Exception exc)
+            {
+                Logger.Error("error handling message", exc);
+            }
+        }
+
+        protected void ActuallyHandleMessage(Action<string> respond, IrcEventArgs args, MessageFlags flags)
+        {
+            if (flags.HasFlag(MessageFlags.UserBanned))
+            {
+                return;
+            }
+
+            var message = args.Data;
+            if (message.Nick == ConnectionManager.Client.Nickname)
+            {
+                return;
+            }
+
+            var lowerBody = message.Message.ToLowerInvariant();
+
+            if (Config.CommandsResponses.ContainsKey(lowerBody))
+            {
+                Logger.DebugFormat("{0} triggered {1} in {2}", message.Nick, lowerBody, message.Channel);
+                respond(Config.CommandsResponses[lowerBody]);
+            }
+        }
+    }
+}
