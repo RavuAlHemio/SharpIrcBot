53a7f15665515a113655d9359c42e3438325982d
Ondřej Hošek <ondra.hosek@gmail.com>
2016-01-07 23:07:31 +0100

    add Punt module

---
+++ Plugins/Punt/PuntConfig.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Punt
+{
+    [JsonObject(MemberSerialization.OptOut)]
+    public class PuntConfig
+    {
+        public Dictionary<string, string> Patterns { get; set; }
+
+        public PuntConfig(JObject obj)
+        {
+            Patterns = new Dictionary<string, string>();
+
+            var ser = new JsonSerializer();
+            ser.Populate(obj.CreateReader(), this);
+        }
+    }
+}
---
+++ Plugins/Punt/PuntPlugin.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using log4net;
+using Meebey.SmartIrc4net;
+using Newtonsoft.Json.Linq;
+using SharpIrcBot;
+
+namespace Punt
+{
+    public class PuntPlugin : IPlugin
+    {
+        private static readonly ILog Logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        protected ConnectionManager ConnectionManager { get; }
+        protected PuntConfig Config { get; }
+        protected Dictionary<string, Regex> RegexCache { get; }
+
+        public PuntPlugin(ConnectionManager connMgr, JObject config)
+        {
+            ConnectionManager = connMgr;
+            Config = new PuntConfig(config);
+            RegexCache = new Dictionary<string, Regex>(Config.Patterns.Count);
+
+            ConnectionManager.ChannelAction += HandleChannelAction;
+            ConnectionManager.ChannelMessage += HandleChannelMessage;
+            ConnectionManager.ChannelNotice += HandleChannelNotice;
+        }
+
+        protected void HandleChannelAction(object sender, ActionEventArgs e, MessageFlags flags)
+        {
+            try
+            {
+                ActuallyHandleMessage(e.Data.Channel, e.Data.Nick, e.ActionMessage);
+            }
+            catch (Exception exc)
+            {
+                Logger.Error("error handling channel action", exc);
+            }
+        }
+
+        protected void HandleChannelMessage(object sender, IrcEventArgs e, MessageFlags flags)
+        {
+            try
+            {
+                ActuallyHandleMessage(e.Data.Channel, e.Data.Nick, e.Data.Message);
+            }
+            catch (Exception exc)
+            {
+                Logger.Error("error handling channel message", exc);
+            }
+        }
+
+        protected void HandleChannelNotice(object sender, IrcEventArgs e, MessageFlags flags)
+        {
+            try
+            {
+                ActuallyHandleMessage(e.Data.Channel, e.Data.Nick, e.Data.Message);
+            }
+            catch (Exception exc)
+            {
+                Logger.Error("error handling channel notice", exc);
+            }
+        }
+
+        protected virtual void ActuallyHandleMessage(string channel, string nick, string body)
+        {
+            foreach (var kvp in Config.Patterns)
+            {
+                if (!RegexCache.ContainsKey(kvp.Key))
+                {
+                    RegexCache[kvp.Key] = new Regex(kvp.Key);
+                }
+
+                if (RegexCache[kvp.Key].IsMatch(body))
+                {
+                    // match! kick 'em!
+                    ConnectionManager.Client.RfcKick(channel, nick, kvp.Value);
+                    return;
+                }
+            }
+        }
+    }
+}
