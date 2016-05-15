23d10bc1b2066e0b264b9f43663ebc2bf05b3d53
Ondřej Hošek <ondra.hosek@gmail.com>
2016-05-15 15:50:51 +0200

    add LinkInfoTests, testing heuristic URI detection

---
+++ Tests/LinkInfoTests/HeuristicLinkDetectorTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LinkInfo;
+using Xunit;
+
+namespace LinkInfoTests
+{
+    public class HeuristicLinkDetectorTests
+    {
+        [Fact]
+        public void TestDomainDetection()
+        {
+            // short list for testing purposes
+            var tlds = new HashSet<string>
+            {
+                "com",
+                "org",
+                "edu",
+                "mil",
+                "gov",
+                "net"
+            };
+            var detector = new HeuristicLinkDetector(tlds);
+
+            AssertValidUri(detector, "omg.org", "http://omg.org/");
+            AssertInvalidUri(detector, "omg.aero");
+            AssertValidUri(detector, "upload.wikimedia.org/wikipedia/commons/c/c5/Logo_FC_Bayern_M%C3%BCnchen.svg", "http://upload.wikimedia.org/wikipedia/commons/c/c5/Logo_FC_Bayern_M%C3%BCnchen.svg");
+        }
+
+        private void AssertValidUri(HeuristicLinkDetector detector, string input, string absoluteUri)
+        {
+            Uri uri;
+            Assert.True(detector.TryCreateUri(input, out uri));
+            Assert.Equal(absoluteUri, uri.AbsoluteUri);
+        }
+
+        private void AssertInvalidUri(HeuristicLinkDetector detector, string input)
+        {
+            Uri uri;
+            Assert.False(detector.TryCreateUri(input, out uri));
+        }
+    }
+}
--- Tests/runtests.sh
+++ Tests/runtests.sh
@@ -3,6 +3,7 @@ mono \
   ./testrunner/xunit.runner.console.*/tools/xunit.console.exe \
   Tests/SharpIrcBotTests/bin/Release/SharpIrcBotTests.dll \
   Tests/RegexTests/bin/Release/RegexTests.dll \
+  Tests/LinkInfoTests/bin/Release/LinkInfoTests.dll \
   -nocolor \
   -noappdomain \
   -parallel none
