9cb22afc40ceb3cea97541d5a1257af200d3b1fc
Ondřej Hošek <ondra.hosek@gmail.com>
2016-10-23 17:47:36 +0200

    new plugin WhoDoYouThinkIs: find out the registered nickname for a user

---
+++ Plugins/WhoDoYouThinkIs/WhoDoYouThinkIsPlugin.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using log4net;
+using Newtonsoft.Json.Linq;
+using SharpIrcBot;
+using SharpIrcBot.Events.Irc;
+
+namespace WhoDoYouThinkIs
+{
+    public class WhoDoYouThinkIsPlugin
+    {
+        private static readonly ILog Logger = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public static readonly Regex WhoDoYouThinkIsRegex = new Regex("^!wdyti\\s+(?<nick>[^ ]+)\\s*$", RegexOptions.Compiled);
+
+        protected IConnectionManager ConnectionManager { get; }
+
+        public WhoDoYouThinkIsPlugin(IConnectionManager connMgr, JObject config)
+        {
+            ConnectionManager = connMgr;
+
+            ConnectionManager.ChannelMessage += HandleChannelMessage;
+            ConnectionManager.QueryMessage += HandleQueryMessage;
+        }
+
+        private void HandleQueryMessage(object sender, IPrivateMessageEventArgs e, MessageFlags flags)
+        {
+            HandleMessage(
+                e.Message,
+                flags,
+                msg => ConnectionManager.SendQueryMessage(e.SenderNickname, msg)
+            );
+        }
+
+        private void HandleChannelMessage(object sender, IChannelMessageEventArgs e, MessageFlags flags)
+        {
+            HandleMessage(
+                e.Message,
+                flags,
+                msg => ConnectionManager.SendChannelMessage(e.Channel, $"{e.SenderNickname}: {msg}")
+            );
+        }
+
+        private void HandleMessage(string messageBody, MessageFlags flags, Action<string> respond)
+        {
+            if (flags.HasFlag(MessageFlags.UserBanned))
+            {
+                return;
+            }
+
+            Match wdytiMatch = WhoDoYouThinkIsRegex.Match(messageBody);
+            if (!wdytiMatch.Success)
+            {
+                return;
+            }
+
+            string nick = wdytiMatch.Groups["nick"].Value;
+            string regName = ConnectionManager.RegisteredNameForNick(nick);
+
+            respond(regName == null ? $"I don't know {nick}." : $"I think {nick} is {regName}.");
+        }
+    }
+}
---
+++ Tests/RegexTests/WhoDoYouThinkIsTests.cs
@@ -0,0 +1,48 @@
+﻿using WhoDoYouThinkIs;
+using Xunit;
+
+namespace RegexTests
+{
+    public class WhoDoYouThinkIsTests
+    {
+        private static void TestWdytiRegexValid(string testString, string nick)
+        {
+            var match = WhoDoYouThinkIsPlugin.WhoDoYouThinkIsRegex.Match(testString);
+            Assert.True(match.Success);
+            Assert.True(match.Groups["nick"].Success);
+            Assert.Equal(nick, match.Groups["nick"].Value);
+        }
+
+        private static void TestWdytiRegexInvalid(string testString)
+        {
+            Assert.False(WhoDoYouThinkIsPlugin.WhoDoYouThinkIsRegex.IsMatch(testString));
+        }
+
+        [Fact]
+        public void TestWdytiRegex()
+        {
+            foreach (var testString in RegexTestUtils.SpaceOut("!wdyti", "RavuAlHemio"))
+            {
+                TestWdytiRegexValid(testString, "RavuAlHemio");
+            }
+
+            // missing argument
+            foreach (var testString in RegexTestUtils.SpaceOut("!wdyti"))
+            {
+                TestWdytiRegexInvalid(testString);
+            }
+
+            // supernumerary argument
+            foreach (var testString in RegexTestUtils.SpaceOut("!wdyti", "RavuAlHemio", "RavusBot"))
+            {
+                TestWdytiRegexInvalid(testString);
+            }
+
+            // unspaced argument
+            foreach (var testString in RegexTestUtils.SpaceOut("!wdytiRavuAlHemio"))
+            {
+                TestWdytiRegexInvalid(testString);
+            }
+        }
+    }
+}
