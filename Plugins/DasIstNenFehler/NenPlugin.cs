29f37e6b9e9b1e16979d04c1215a18411f53d16e
Ondřej Hošek <hosek@edvlab.tuwien.ac.at>
2016-12-12 19:59:27 +0100

    Nen: store database in memory

--- Plugins/DasIstNenFehler/NenPlugin.cs
+++ Plugins/DasIstNenFehler/NenPlugin.cs
@@ -16,6 +16,10 @@ namespace DasIstNenFehler
 
         public static readonly ImmutableHashSet<char> GermanAlphabet = ImmutableHashSet.CreateRange("ßqwertzuiopüasdfghjklöäyxcvbnmQWERTZUIOPÜASDFGHJKLÖÄYXCVBNM");
 
+        protected ImmutableDictionary<long, string> IDsToWords { get; set; }
+        protected ImmutableDictionary<string, ImmutableList<Noun>> LowerWordToNouns { get; set; }
+        protected ImmutableDictionary<string, ImmutableList<Adjective>> LowerWordToAdjectives { get; set; }
+
         protected IConnectionManager ConnectionManager { get; }
         protected NenConfig Config { get; set; }
 
@@ -24,6 +28,8 @@ namespace DasIstNenFehler
             ConnectionManager = connMgr;
             Config = new NenConfig(config);
 
+            LoadWords();
+
             ConnectionManager.ChannelMessage += HandleChannelMessageOrAction;
             ConnectionManager.ChannelAction += HandleChannelMessageOrAction;
         }
@@ -31,6 +37,54 @@ namespace DasIstNenFehler
         public void ReloadConfiguration(JObject newConfig)
         {
             Config = new NenConfig(newConfig);
+
+            LoadWords();
+        }
+
+        protected virtual void LoadWords()
+        {
+            using (var ctx = GetNewContext())
+            {
+                IDsToWords = ImmutableDictionary.CreateRange(
+                    ctx.Words.ToList().Select(kvp => CreateKVP(kvp.ID, kvp.WordString))
+                );
+
+                var wordsToNouns = new Dictionary<string, List<Noun>>();
+                foreach (Noun noun in ctx.Nouns)
+                {
+                    string lowerWord = IDsToWords[noun.WordID].ToLowerInvariant();
+
+                    List<Noun> existingNouns;
+                    if (!wordsToNouns.TryGetValue(lowerWord, out existingNouns))
+                    {
+                        existingNouns = new List<Noun>();
+                        wordsToNouns[lowerWord] = existingNouns;
+                    }
+
+                    existingNouns.Add(noun);
+                }
+                LowerWordToNouns = ImmutableDictionary.CreateRange(
+                    wordsToNouns.Select(kvp => CreateKVP(kvp.Key, ImmutableList.CreateRange(kvp.Value)))
+                );
+
+                var wordsToAdjectives = new Dictionary<string, List<Adjective>>();
+                foreach (Adjective adj in ctx.Adjectives)
+                {
+                    string lowerWord = IDsToWords[adj.WordID].ToLowerInvariant();
+
+                    List<Adjective> existingAdjectives;
+                    if (!wordsToAdjectives.TryGetValue(lowerWord, out existingAdjectives))
+                    {
+                        existingAdjectives = new List<Adjective>();
+                        wordsToAdjectives[lowerWord] = existingAdjectives;
+                    }
+
+                    existingAdjectives.Add(adj);
+                }
+                LowerWordToAdjectives = ImmutableDictionary.CreateRange(
+                    wordsToAdjectives.Select(kvp => CreateKVP(kvp.Key, ImmutableList.CreateRange(kvp.Value)))
+                );
+            }
         }
 
         protected virtual void HandleChannelMessageOrAction(object sender, IChannelMessageEventArgs e, MessageFlags flags)
@@ -86,114 +140,115 @@ namespace DasIstNenFehler
                 }
 
                 // find the word
-                using (var ctx = GetNewContext())
+                string searchWord = lastHyphenatedChunk;
+                Word word;
+                while (searchWord.Length > 0)
                 {
-                    string searchWord = lastHyphenatedChunk;
-                    Word word;
-                    while (searchWord.Length > 0)
+                    ImmutableList<Noun> nouns;
+                    ImmutableList<Adjective> adjectives;
+                    if (!LowerWordToNouns.TryGetValue(searchWord.ToLowerInvariant(), out nouns))
                     {
-                        word = ctx.Words
-                            .FirstOrDefault(w => w.WordString.ToLowerInvariant() == searchWord.ToLowerInvariant());
-                        searchWord = searchWord.Substring(1);
+                        nouns = null;
+                    }
+                    if (!LowerWordToAdjectives.TryGetValue(searchWord.ToLowerInvariant(), out adjectives))
+                    {
+                        adjectives = null;
+                    }
 
-                        if (word == null)
+                    string thisSearchWord = searchWord;
+                    searchWord = searchWord.Substring(1);
+
+                    if (nouns == null && adjectives == null)
+                    {
+                        // not found
+                        continue;
+                    }
+
+                    // found it! is it a noun?
+                    if (nouns != null)
+                    {
+                        // yes!
+
+                        // is any of those nouns masculine singular accusative?
+                        Noun correctNoun = nouns
+                            .Where(n => n.Case == GrammaticalCase.Accusative)
+                            .Where(n => n.Number == GrammaticalNumber.Singular)
+                            .Where(n => n.Gender == GrammaticalGender.Masculine)
+                            .FirstOrDefault()
+                        ;
+                        if (correctNoun != null)
                         {
-                            // not found
-                            continue;
+                            correctUsage = true;
+
+                            string baseWord = IDsToWords[correctNoun.BaseWordID];
+                            NotifyUsers($"NenPlugin: {e.SenderNickname} in {e.Channel}: \"nen {lastHyphenatedChunk}\" trimmed to \"{thisSearchWord}\" matches noun {baseWord}");
                         }
 
-                        // found it! is it a noun?
-                        List<Noun> nouns = ctx.Nouns
-                            .Where(w => w.WordID == word.ID)
-                            .ToList();
-                        if (nouns.Count != 0)
+                        // is any of them masculine singular nominative?
+                        Noun potentiallyIncorrectNoun = nouns
+                            .Where(n => n.Case == GrammaticalCase.Nominative)
+                            .Where(n => n.Number == GrammaticalNumber.Singular)
+                            .Where(n => n.Gender == GrammaticalGender.Masculine)
+                            .FirstOrDefault()
+                        ;
+                        if (potentiallyIncorrectNoun != null)
                         {
-                            // yes!
-
-                            // is any of those nouns masculine singular accusative?
-                            Noun correctNoun = nouns
-                                .Where(n => n.Case == GrammaticalCase.Accusative)
-                                .Where(n => n.Number == GrammaticalNumber.Singular)
-                                .Where(n => n.Gender == GrammaticalGender.Masculine)
-                                .FirstOrDefault()
-                            ;
-                            if (correctNoun != null)
-                            {
-                                correctUsage = true;
-
-                                string baseWord = ctx.Words.FirstOrDefault(w => w.ID == correctNoun.BaseWordID).WordString;
-                                NotifyUsers($"NenPlugin: {e.SenderNickname} in {e.Channel}: \"nen {lastHyphenatedChunk}\" trimmed to \"{word.WordString}\" matches noun {baseWord}");
-                            }
-
-                            // is any of them masculine singular nominative?
-                            Noun potentiallyIncorrectNoun = nouns
-                                .Where(n => n.Case == GrammaticalCase.Nominative)
-                                .Where(n => n.Number == GrammaticalNumber.Singular)
-                                .Where(n => n.Gender == GrammaticalGender.Masculine)
-                                .FirstOrDefault()
-                            ;
-                            if (potentiallyIncorrectNoun != null)
-                            {
-                                potentiallyIncorrectUsage = true;
-                            }
-                        }
-
-                        // is it an adjective?
-                        List<Adjective> adjs = ctx.Adjectives
-                            .Where(w => w.WordID == word.ID)
-                            .ToList();
-                        if (adjs.Count != 0)
-                        {
-                            // is any of those adjectives masculine singular accusative?
-                            Adjective correctAdjective = adjs
-                                .Where(n => n.Case == GrammaticalCase.Accusative)
-                                .Where(n => n.Number == GrammaticalNumber.Singular)
-                                .Where(n => n.Gender == GrammaticalGender.Masculine)
-                                .FirstOrDefault()
-                            ;
-                            if (correctAdjective != null)
-                            {
-                                correctUsage = true;
-
-                                string baseWord = ctx.Words.FirstOrDefault(w => w.ID == correctAdjective.BaseWordID).WordString;
-                                NotifyUsers($"NenPlugin: {e.SenderNickname} in {e.Channel}: \"nen {lastHyphenatedChunk}\" trimmed to \"{word.WordString}\" matches adjective {baseWord}");
-                            }
-
-                            // is any of them masculine singular nominative?
-                            Adjective potentiallyIncorrectAdjective = adjs
-                                .Where(n => n.Case == GrammaticalCase.Nominative)
-                                .Where(n => n.Number == GrammaticalNumber.Singular)
-                                .Where(n => n.Gender == GrammaticalGender.Masculine)
-                                .FirstOrDefault()
-                            ;
-                            if (potentiallyIncorrectAdjective != null)
-                            {
-                                potentiallyIncorrectUsage = true;
-                            }
-                        }
-
-                        if (potentiallyIncorrectUsage)
-                        {
-                            if (!correctUsage)
-                            {
-                                NotifyUsers($"NenPlugin: {e.SenderNickname} in {e.Channel}: \"nen {lastHyphenatedChunk}\" is incorrect");
-                            }
-
-                            // correct usage
-                            return;
-                        }
-                        else if (nouns.Count + adjs.Count > 0)
-                        {
-                            NotifyUsers($"NenPlugin: {e.SenderNickname} in {e.Channel}: \"nen {lastHyphenatedChunk}\" might be incorrect");
-                            return;
+                            potentiallyIncorrectUsage = true;
                         }
                     }
 
-                    // possibly TODO: try matching the next word instead
-                    // for the time being, assume it's wrong
-                    NotifyUsers($"NenPlugin: {e.SenderNickname} in {e.Channel}: \"nen {lastHyphenatedChunk}\" is unknown");
-                    break;
+                    // is it an adjective?
+                    if (adjectives != null)
+                    {
+                        // is any of those adjectives masculine singular accusative?
+                        Adjective correctAdjective = adjectives
+                            .Where(n => n.Case == GrammaticalCase.Accusative)
+                            .Where(n => n.Number == GrammaticalNumber.Singular)
+                            .Where(n => n.Gender == GrammaticalGender.Masculine)
+                            .FirstOrDefault()
+                        ;
+                        if (correctAdjective != null)
+                        {
+                            correctUsage = true;
+
+                            string baseWord = IDsToWords[correctAdjective.BaseWordID];
+                            NotifyUsers($"NenPlugin: {e.SenderNickname} in {e.Channel}: \"nen {lastHyphenatedChunk}\" trimmed to \"{thisSearchWord}\" matches adjective {baseWord}");
+                        }
+
+                        // is any of them masculine singular nominative?
+                        Adjective potentiallyIncorrectAdjective = adjectives
+                            .Where(n => n.Case == GrammaticalCase.Nominative)
+                            .Where(n => n.Number == GrammaticalNumber.Singular)
+                            .Where(n => n.Gender == GrammaticalGender.Masculine)
+                            .FirstOrDefault()
+                        ;
+                        if (potentiallyIncorrectAdjective != null)
+                        {
+                            potentiallyIncorrectUsage = true;
+                        }
+                    }
+
+                    if (potentiallyIncorrectUsage)
+                    {
+                        if (!correctUsage)
+                        {
+                            NotifyUsers($"NenPlugin: {e.SenderNickname} in {e.Channel}: \"nen {lastHyphenatedChunk}\" is incorrect");
+                        }
+
+                        // correct usage
+                        return;
+                    }
+                    else if (nouns.Count + adjectives.Count > 0)
+                    {
+                        NotifyUsers($"NenPlugin: {e.SenderNickname} in {e.Channel}: \"nen {lastHyphenatedChunk}\" might be incorrect");
+                        return;
+                    }
                 }
+
+                // possibly TODO: try matching the next word instead
+                // for the time being, assume it's wrong
+                NotifyUsers($"NenPlugin: {e.SenderNickname} in {e.Channel}: \"nen {lastHyphenatedChunk}\" is unknown");
+                break;
             }
         }
 
@@ -209,6 +264,12 @@ namespace DasIstNenFehler
         {
             var opts = SharpIrcBotUtil.GetContextOptions<GermanWordsContext>(Config);
             return new GermanWordsContext(opts);
+        }
+
+        // {CreateKVP("one", 2)} is shorter than {new KeyValuePair<string, int>("one", 2)}
+        protected static KeyValuePair<TKey, TValue> CreateKVP<TKey, TValue>(TKey key, TValue value)
+        {
+            return new KeyValuePair<TKey, TValue>(key, value);
         }
     }
 }
