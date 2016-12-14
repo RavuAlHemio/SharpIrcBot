edcc5230434cbb4e34344b7f1f8c26040dd0cbfc
Ondřej Hošek <hosek@edvlab.tuwien.ac.at>
2016-12-14 12:15:59 +0100

    SharpIrcBot: improve plugin load debugging

--- SharpIrcBot/PluginManager.cs
+++ SharpIrcBot/PluginManager.cs
@@ -43,7 +43,7 @@ namespace SharpIrcBot
                 var type = ass.GetType(plugin.Class);
                 if (!typeof(IPlugin).GetTypeInfo().IsAssignableFrom(type))
                 {
-                    throw new ArgumentException("class is not a plugin");
+                    throw new ArgumentException($"class {plugin.Class} from assembly {plugin.Assembly} is not a plugin (does not implement {typeof(IPlugin).FullName})");
                 }
                 var ctor = type.GetTypeInfo().GetConstructor(new [] {typeof(IConnectionManager), typeof(JObject)});
                 var pluginObject = (IPlugin)ctor.Invoke(new object[] {connManager, plugin.Config});
