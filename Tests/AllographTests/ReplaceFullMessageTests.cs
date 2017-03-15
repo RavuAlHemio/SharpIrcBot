30e39a844cda925f60f8bcf0353e4a35529a1649
Ondřej Hošek <ondra.hosek@gmail.com>
2017-03-15 20:24:06 +0100

    add test for full-message replacement
    Turns out it works correctly (the replacement of the first chunk is
    taken to be the full message); the configured regex was incorrect in this
    regard.
    
    Closes #12.

---
+++ Tests/AllographTests/ReplaceFullMessageTests.cs
@@ -0,0 +1,52 @@
+using Allograph;
+using LinkInfo;
+using Newtonsoft.Json.Linq;
+using SharpIrcBot.TestPlumbing;
+using SharpIrcBot.TestPlumbing.Events.Logging;
+using Xunit;
+
+namespace AllographTests
+{
+    public class ReplaceFullMessageTests
+    {
+        [Fact]
+        public void TestReplaceFullMessage()
+        {
+            const string constRegex = "(?i)^\\s*(?:wei(?:ss|sz|\u00DF)(?:\\s+hier)?\\s+(?:irgend)?(?:jemand|wer|einer?)|wei(?:ss|sz|\u00DF)t\\s+du|wisst\\s+ihr|wissen\\s+(?:sie|wir))\\s+.*(?:wer|von|ob|warum|wieso|weshalb|wo|was|wann|wessen|wie)\\s+.+$";
+            const string constChannelName = "#test";
+            const string constTrigger = "Wisst ihr was ich an http://ondrahosek.com am meisten hasse? ";
+            const string constHumAFewBars = "Nein, aber wenn du ein paar Takte vorsummst kann ich so tun als ob.";
+
+            var connMgr = new TestConnectionManager();
+            var moduleConfig = new JObject
+            {
+                ["ProbabilityPercent"] = new JValue(100.0),
+                ["Replacements"] = new JArray
+                {
+                    new JObject
+                    {
+                        ["RegexString"] = new JValue(constRegex),
+                        ["ReplacementString"] = new JValue(constHumAFewBars),
+                        ["ReplaceFullMessage"] = new JValue(true)
+                    }
+                }
+            };
+
+            // for chunk splitting
+            var splitter = new LinkInfoPlugin(connMgr, new JObject());
+            var allograph = new AllographPlugin(connMgr, moduleConfig);
+
+            connMgr.InjectChannelMessage(
+                constChannelName,
+                "User",
+                constTrigger
+            );
+
+            Assert.Equal(1, connMgr.EventLog.Count);
+            var sentMessage = Assert.IsType<TestMessage>(connMgr.EventLog[0]);
+            Assert.Equal(MessageType.Message, sentMessage.Type);
+            Assert.Equal(constChannelName, sentMessage.Target);
+            Assert.Equal(constHumAFewBars, sentMessage.Body);
+        }
+    }
+}
