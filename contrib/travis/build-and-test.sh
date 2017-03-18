c3b629a97565d87013611d609e2c43d3c8b311a1
Ondřej Hošek <ondra.hosek@gmail.com>
2017-03-18 13:08:23 +0100

    travis: do not build .NET Framework versions of projects

--- contrib/travis/build-and-test.sh
+++ contrib/travis/build-and-test.sh
@@ -1,9 +1,27 @@
 #!/bin/sh
 set -e
-dotnet build
-for testdir in Tests/*Tests
+topdir="`pwd`"
+
+cd "$topdir/Libraries/HtmlAgilityPack"
+dotnet build -f "netstandard1.6"
+cd "$topdir/Libraries/SmartIrc4net"
+dotnet build -f "netstandard1.3"
+
+cd "$topdir/SharpIrcBot"
+dotnet build -f "netstandard1.6"
+
+for plugindir in "$topdir/Plugins"/*
+do
+    cd "$plugindir"
+    dotnet build -f "netstandard1.6"
+done
+
+cd "$topdir/SharpIrcBotCLI"
+dotnet build -f "netcoreapp1.1"
+
+for testdir in "$topdir/Tests"/*Tests
 do
     cd "$testdir"
-    dotnet test
-    cd "../.."
+    dotnet build -f "netcoreapp1.1"
+    dotnet test -f "netcoreapp1.1"
 done
