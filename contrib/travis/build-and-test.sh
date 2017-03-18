4a779fa560c8660da8255db8ef0b46f559fed7a0
Ondřej Hošek <ondra.hosek@gmail.com>
2017-03-18 13:12:34 +0100

    travis: specify project file names explicitly where expedient

--- contrib/travis/build-and-test.sh
+++ contrib/travis/build-and-test.sh
@@ -3,12 +3,12 @@ set -e
 topdir="`pwd`"
 
 cd "$topdir/Libraries/HtmlAgilityPack"
-dotnet build -f "netstandard1.6"
+dotnet build "HtmlAgilityPack.csproj" -f "netstandard1.6"
 cd "$topdir/Libraries/SmartIrc4net"
-dotnet build -f "netstandard1.3"
+dotnet build "SmartIrc4net.csproj" -f "netstandard1.3"
 
 cd "$topdir/SharpIrcBot"
-dotnet build -f "netstandard1.6"
+dotnet build "SharpIrcBot.csproj" -f "netstandard1.6"
 
 for plugindir in "$topdir/Plugins"/*
 do
@@ -17,7 +17,7 @@ do
 done
 
 cd "$topdir/SharpIrcBotCLI"
-dotnet build -f "netcoreapp1.1"
+dotnet build "SharpIrcBotCLI.csproj" -f "netcoreapp1.1"
 
 for testdir in "$topdir/Tests"/*Tests
 do
