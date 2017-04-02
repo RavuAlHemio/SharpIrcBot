3815d556641aa2d99fbb5b57ad9d12f9e62cec62
Ondřej Hošek <ondra.hosek@gmail.com>
2017-04-02 23:27:08 +0200

    UnicodeStringBuilder: allow "skip" characters (-1, no output)

--- SharpIrcBot/UnicodeStringBuilder.cs
+++ SharpIrcBot/UnicodeStringBuilder.cs
@@ -8,6 +8,33 @@ namespace SharpIrcBot
     {
         protected List<int> Characters { get; set; }
 
+        protected bool ActualAllowSkipCharacters { get; set; }
+
+        public bool AllowSkipCharacters
+        {
+            get
+            {
+                return ActualAllowSkipCharacters;
+            }
+            set
+            {
+                if (!value)
+                {
+                    // verify
+                    int indexOfSkip = Characters.IndexOf(-1);
+                    if (indexOfSkip != -1)
+                    {
+                        throw new ArgumentException(
+                            $"character list already contains skip at position {indexOfSkip}",
+                            nameof(value)
+                        );
+                    }
+                }
+
+                ActualAllowSkipCharacters = value;
+            }
+        }
+
         public UnicodeStringBuilder()
         {
             Characters = new List<int>();
@@ -15,12 +42,22 @@ namespace SharpIrcBot
 
         public UnicodeStringBuilder(string baseString)
         {
+            if (baseString == null)
+            {
+                throw new ArgumentNullException(nameof(baseString));
+            }
+
             Characters = new List<int>(baseString.Length);
             Append(baseString);
         }
 
         public UnicodeStringBuilder(int initialCapacity)
         {
+            if (initialCapacity < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(initialCapacity));
+            }
+
             Characters = new List<int>(initialCapacity);
         }
 
@@ -28,6 +65,11 @@ namespace SharpIrcBot
 
         public virtual void Append(string str)
         {
+            if (str == null)
+            {
+                throw new ArgumentNullException(nameof(str));
+            }
+
             int i = 0;
             while (i < str.Length)
             {
@@ -51,6 +93,11 @@ namespace SharpIrcBot
 
         public virtual void AppendLine(string line)
         {
+            if (line == null)
+            {
+                throw new ArgumentNullException(nameof(line));
+            }
+
             Append(line);
             AppendLine();
         }
@@ -65,33 +112,83 @@ namespace SharpIrcBot
             Append(string.Format(provider, format, args));
         }
 
+        public virtual void RemoveAt(int index)
+        {
+            Characters.RemoveAt(index);
+        }
+
+        public virtual void RemoveRange(int index, int count)
+        {
+            Characters.RemoveRange(index, count);
+        }
+
         public int this[int i]
         {
             get
             {
+                if (i < 0 || i >= Characters.Count)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(i));
+                }
+
                 return Characters[i];
             }
             set
             {
+                if (i < 0 || i >= Characters.Count)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(i));
+                }
+                if (value < -1)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value));
+                }
+                if (!ActualAllowSkipCharacters && value == -1)
+                {
+                    throw new ArgumentOutOfRangeException(
+                        nameof(value),
+                        value,
+                        $"the value -1 is only allowed if {nameof(AllowSkipCharacters)} is true"
+                    );
+                }
+
                 Characters[i] = value;
             }
         }
 
         public string CharAtAsString(int i)
         {
+            if (Characters[i] == -1)
+            {
+                return "";
+            }
             return char.ConvertFromUtf32(Characters[i]);
         }
 
         public void SetCharAtToString(int i, string s)
         {
+            if (s.Length == 0)
+            {
+                if (!ActualAllowSkipCharacters)
+                {
+                    throw new ArgumentException(
+                        $"An empty string is only allowed if {nameof(AllowSkipCharacters)} is true",
+                        nameof(s)
+                    );
+                }
+                Characters[i] = -1;
+                return;
+            }
+
             if (
                 (char.IsHighSurrogate(s[0]) && s.Length != 2) ||
                 (!char.IsHighSurrogate(s[0]) && s.Length != 1)
             )
             {
                 throw new ArgumentException(
-                    "The string must be either one character (Basic Multilingual Plane) or two characters " +
-                    "(Supplemental Multilingual Planes) long",
+                    $"The string must be either zero characters (skip character; iff {nameof(AllowSkipCharacters)} is " +
+                    "true), one character (Basic Multilingual Plane) or two characters (Supplemental Multilingual " +
+                    "Planes) long",
                     nameof(s)
                 );
             }
@@ -104,6 +201,12 @@ namespace SharpIrcBot
             var builder = new StringBuilder(Characters.Count * 2);
             foreach (int c in Characters)
             {
+                if (c == -1)
+                {
+                    // skip
+                    continue;
+                }
+
                 builder.Append(char.ConvertFromUtf32(c));
             }
             return builder.ToString();
