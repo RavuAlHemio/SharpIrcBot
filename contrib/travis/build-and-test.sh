08a63493127ddd85601baabfbd6994fd094979b0
Ondřej Hošek <ondra.hosek@gmail.com>
2018-06-09 15:52:33 +0200

    travis: streamline build-and-test.sh
    Package restoration is part of the regular build process now, and dotnet is better at guessing
    which framework to use for dependencies.

--- contrib/travis/build-and-test.sh
+++ contrib/travis/build-and-test.sh
@@ -3,32 +3,6 @@ set -e
 set -x
 topdir="`pwd`"
 
-cd "$topdir/Libraries/HtmlAgilityPack"
-dotnet build "HtmlAgilityPack.csproj" -f "netstandard1.6"
-cd "$topdir/Libraries/SmartIrc4net"
-dotnet build "SmartIrc4net.csproj" -f "netstandard1.3"
-
-cd "$topdir/SharpIrcBot"
-dotnet build "SharpIrcBot.csproj" -f "netstandard2.0"
-
-for plugindir in "$topdir/Plugins"/*
-do
-    cd "$plugindir"
-    if [ -f *.csproj ]
-    then
-        dotnet build -f "netstandard2.0"
-    fi
-done
-
-for plugindir in "$topdir/Plugins/Libraries"/*
-do
-    cd "$plugindir"
-    if [ -f *.csproj ]
-    then
-        dotnet build -f "netstandard2.0"
-    fi
-done
-
 cd "$topdir/SharpIrcBotCLI"
 dotnet build "SharpIrcBotCLI.csproj" -f "netcoreapp2.1"
 
