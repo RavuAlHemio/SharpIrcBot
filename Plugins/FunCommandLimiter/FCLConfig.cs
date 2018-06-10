6d66f62f0ad646d4b3af27b38f2b2d5a05f417b2
Ondřej Hošek <ondra.hosek@gmail.com>
2018-06-10 17:18:50 +0200

    new plugin FunCommandLimiter
    Limits "fun commands" (commands with specific, admin-choosable tags) to
    only be used a specific number of times per unit of time, optionally
    differentiating by channel and/or user.

---
+++ Plugins/FunCommandLimiter/FCLConfig.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace SharpIrcBot.Plugins.FunCommandLimiter
+{
+    [JsonObject(MemberSerialization.OptOut)]
+    public class FCLConfig
+    {
+        public HashSet<string> CommandTags { get; set; }
+        public double TimeSpanMinutes { get; set; }
+        public int MaxCountPerTime { get; set; }
+        public HashSet<string> Channels { get; set; }
+        public bool PerUser { get; set; }
+        public bool PerChannel { get; set; }
+        public bool CountPrivateMessages { get; set; }
+        public bool CountLimitedAttempts { get; set; }
+
+        public FCLConfig(JObject obj)
+        {
+            CommandTags = new HashSet<string>();
+            TimeSpanMinutes = 1440.0; // 1d
+            MaxCountPerTime = 50;
+            Channels = new HashSet<string>();
+            PerUser = true;
+            PerChannel = true;
+            CountPrivateMessages = false;
+            CountLimitedAttempts = true;
+
+            var ser = new JsonSerializer();
+            ser.Populate(obj.CreateReader(), this);
+        }
+    }
+}
---
+++ Plugins/FunCommandLimiter/FunCommandLimiterPlugin.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
+using SharpIrcBot;
+using SharpIrcBot.Commands;
+using SharpIrcBot.Events.Irc;
+using SharpIrcBot.Util;
+
+namespace SharpIrcBot.Plugins.FunCommandLimiter
+{
+    /// <summary>
+    /// Limit the number of "fun commands" being used in a channel per unit of time.
+    /// </summary>
+    public class FunCommandLimiterPlugin : IPlugin, IReloadableConfiguration
+    {
+        private static readonly ILogger Logger = LogUtil.LoggerFactory.CreateLogger<FunCommandLimiterPlugin>();
+        
+        public const string GlobalChannelKey = "\r\nGLOBAL";
+        public const string PrivateMessageChannelKey = "\r\nQUERY";
+        public const string GlobalUserKey = "\r\nGLOBAL";
+
+        protected IConnectionManager Connection { get; set; }
+        protected FCLConfig Config { get; set; }
+        protected Dictionary<string, Dictionary<string, List<DateTimeOffset>>> ChannelToUserToTimestamps { get; set; }
+
+        public FunCommandLimiterPlugin(IConnectionManager connMgr, JObject config)
+        {
+            Connection = connMgr;
+            Config = new FCLConfig(config);
+            ChannelToUserToTimestamps = new Dictionary<string, Dictionary<string, List<DateTimeOffset>>>();
+
+            Connection.CommandManager.RegisterGlobalCommandCallback(LimiterCallback);
+        }
+
+        public virtual void ReloadConfiguration(JObject newConfig)
+        {
+            Config = new FCLConfig(newConfig);
+            PostConfigReload();
+        }
+
+        protected virtual void PostConfigReload()
+        {
+        }
+
+        protected virtual bool LimiterCallback(CommandMatch commandMatch, IUserMessageEventArgs msg)
+        {
+            // is it a fun command?
+            if (!commandMatch.Command.Tags.Any(tag => Config.CommandTags.Contains(tag)))
+            {
+                // no => don't protest
+                return true;
+            }
+
+            // how soon is then?
+            DateTimeOffset cutoff = DateTimeOffset.Now.AddMinutes(-Config.TimeSpanMinutes);
+
+            // find the keys
+            string channelKey;
+            if (Config.PerChannel)
+            {
+                var chanMsg = msg as IChannelMessageEventArgs;
+                if (chanMsg != null)
+                {
+                    if (!Config.Channels.Contains(chanMsg.Channel))
+                    {
+                        // we're not policing this channel
+                        return true;
+                    }
+
+                    channelKey = chanMsg.Channel;
+                }
+                else
+                {
+                    // private message
+                    if (!Config.CountPrivateMessages)
+                    {
+                        // we don't bother with those
+                        return true;
+                    }
+
+                    channelKey = PrivateMessageChannelKey;
+                }
+            }
+            else
+            {
+                if (msg is IPrivateMessageEventArgs && !Config.CountPrivateMessages)
+                {
+                    // we don't bother with private messages
+                    return true;
+                }
+
+                channelKey = GlobalChannelKey;
+            }
+
+            string userKey = Config.PerUser
+                ? (Connection.RegisteredNameForNick(msg.SenderNickname) ?? msg.SenderNickname)
+                : GlobalUserKey;
+
+            // obtain the timestamps
+            List<DateTimeOffset> timestamps = ObtainTimeStampsForChannelAndUserKeys(channelKey, userKey);
+
+            // clear out the old ones
+            timestamps.RemoveAll(ts => ts < cutoff);
+
+            if (timestamps.Count > Config.MaxCountPerTime)
+            {
+                // too many
+
+                if (Config.CountLimitedAttempts)
+                {
+                    // count this one even though it failed >;-)
+                    timestamps.Add(DateTimeOffset.Now);
+                }
+
+                return false;
+            }
+
+            // count this one but allow it
+            timestamps.Add(DateTimeOffset.Now);
+            return true;
+        }
+
+        protected List<DateTimeOffset> ObtainTimeStampsForChannelAndUserKeys(string channelKey, string userKey)
+        {
+            Dictionary<string, List<DateTimeOffset>> userToTimestamps;
+            if (!ChannelToUserToTimestamps.TryGetValue(channelKey, out userToTimestamps))
+            {
+                userToTimestamps = new Dictionary<string, List<DateTimeOffset>>();
+                ChannelToUserToTimestamps[channelKey] = userToTimestamps;
+            }
+
+            List<DateTimeOffset> timestamps;
+            if (!userToTimestamps.TryGetValue(userKey, out timestamps))
+            {
+                timestamps = new List<DateTimeOffset>();
+                userToTimestamps[userKey] = timestamps;
+            }
+
+            return timestamps;
+        }
+    }
+}
