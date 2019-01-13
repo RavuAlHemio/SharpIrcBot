9833366dfc2fda8756e050cd9ec6b80cf26af2ab
Ondřej Hošek <ondra.hosek@gmail.com>
2019-01-13 19:38:44 +0100

    Vitals: add reader returning a constant value

---
+++ Plugins/Vitals/ConstantReader.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using SharpIrcBot.Plugins.Vitals;
+using SharpIrcBot.Util;
+
+namespace SharpIrcBot.Plugins.Vitals
+{
+    public class ConstantReader : IVitalsReader
+    {
+        [JsonObject(MemberSerialization.OptOut)]
+        public class ConstantReaderConfig
+        {
+            public string ConstantText { get; set; }
+
+            public ConstantReaderConfig(JObject obj)
+            {
+                var ser = new JsonSerializer();
+                ser.Populate(obj.CreateReader(), this);
+            }
+        }
+
+        private ConstantReaderConfig _config;
+
+        public ConstantReader(JObject options)
+        {
+            _config = new ConstantReaderConfig(options);
+        }
+
+        public string Read()
+        {
+            return _config.ConstantText;
+        }
+    }
+}
