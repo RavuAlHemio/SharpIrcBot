e80b6117637582aea6a7902259e0522af2ae5c97
Ondřej Hošek <ondra.hosek@gmail.com>
2019-01-24 23:46:27 +0100

    Vitals: RandomReader: multiple values, backreferences w/ factor Support multiple values per random vital, as well as backreferences with a factor (e.g. for unit conversion).

--- Plugins/Vitals/RandomReader.cs
+++ Plugins/Vitals/RandomReader.cs
@@ -13,15 +13,30 @@ namespace SharpIrcBot.Plugins.Vitals
     public class RandomReader : IVitalsReader
     {
         [JsonObject(MemberSerialization.OptOut)]
-        public class RandomReaderConfig
+        public class RandomReaderValue
         {
             public decimal MinValue { get; set; }
             public decimal MaxValue { get; set; }
             public decimal Resolution { get; set; }
+            public int ConversionReference { get; set; }
+            public decimal ConversionFactor { get; set; }
+
+            public RandomReaderValue()
+            {
+                ConversionReference = -1;
+            }
+        }
+
+        [JsonObject(MemberSerialization.OptOut)]
+        public class RandomReaderConfig
+        {
+            public List<RandomReaderValue> Values { get; set; }
             public string FormatString { get; set; }
 
             public RandomReaderConfig(JObject obj)
             {
+                Values = new List<RandomReaderValue>();
+
                 var ser = new JsonSerializer();
                 ser.Populate(obj.CreateReader(), this);
             }
@@ -38,11 +53,23 @@ namespace SharpIrcBot.Plugins.Vitals
 
         public string Read()
         {
-            var span = (int)((_config.MaxValue - _config.MinValue) / _config.Resolution);
-            int add = _rng.Next(span);
-            decimal finalValue = _config.MinValue + (add * _config.Resolution);
+            var finalValues = new decimal[_config.Values.Count];
+            for (int i = 0; i < finalValues.Length; ++i)
+            {
+                RandomReaderValue val = _config.Values[i];
+                if (val.ConversionReference >= 0)
+                {
+                    finalValues[i] = finalValues[val.ConversionReference] * val.ConversionFactor;
+                }
+                else
+                {
+                    var span = (int)((val.MaxValue - val.MinValue) / val.Resolution);
+                    int add = _rng.Next(span);
+                    finalValues[i] = val.MinValue + (add * val.Resolution);
+                }
+            }
 
-            return string.Format(_config.FormatString, finalValue);
+            return string.Format(_config.FormatString, finalValues.Cast<object>().ToArray());
         }
     }
 }
