b6d8d66fb2a2499027ad70533b1625ac7c074550
Ondřej Hošek <ondra.hosek@gmail.com>
2020-07-21 13:25:23 +0200

    new plugin: Fortune

---
+++ Plugins/Fortune/FortuneConfig.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace SharpIrcBot.Plugins.Fortune
+{
+    [JsonObject(MemberSerialization.OptOut)]
+    public class FortuneConfig
+    {
+        public string FortuneDirectory { get; set; }
+
+        public FortuneConfig(JObject obj)
+        {
+            var ser = new JsonSerializer();
+            ser.Populate(obj.CreateReader(), this);
+        }
+    }
+}
---
+++ Plugins/Fortune/FortunePlugin.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using SharpIrcBot.Commands;
+using SharpIrcBot.Events.Irc;
+using SharpIrcBot.Util;
+
+namespace SharpIrcBot.Plugins.Fortune
+{
+    public class FortunePlugin : IPlugin, IReloadableConfiguration
+    {
+        protected IConnectionManager ConnectionManager { get; }
+        protected FortuneConfig Config { get; set; }
+        protected ImmutableDictionary<string, ImmutableArray<string>> CategoryToFortunes { get; set; }
+        protected ImmutableArray<string> AllFortunes { get; set; }
+        protected Random RNG { get; set; }
+
+        public FortunePlugin(IConnectionManager connMgr, JObject config)
+        {
+            ConnectionManager = connMgr;
+
+            ConnectionManager.CommandManager.RegisterChannelMessageCommandHandler(
+                new Command(
+                    CommandUtil.MakeNames("fortune"),
+                    CommandUtil.NoOptions,
+                    CommandUtil.MakeArguments(
+                        RestTaker.Instance // fortune type (optional)
+                    ),
+                    CommandUtil.MakeTags("fun"),
+                    forbiddenFlags: MessageFlags.UserBanned
+                ),
+                HandleFortuneCommand
+            );
+
+            CategoryToFortunes = ImmutableDictionary<string, ImmutableArray<string>>.Empty;
+            AllFortunes = ImmutableArray<string>.Empty;
+            RNG = new Random();
+
+            ReloadFortunes();
+        }
+
+        public virtual void ReloadConfiguration(JObject newConfig)
+        {
+            Config = new FortuneConfig(newConfig);
+            PostConfigReload();
+        }
+
+        protected virtual void PostConfigReload()
+        {
+            ReloadFortunes();
+        }
+
+        protected virtual void ReloadFortunes()
+        {
+            var byCategoryBuilder = ImmutableDictionary.CreateBuilder<string, ImmutableArray<string>>();
+            var allBuilder = ImmutableArray.CreateBuilder<string>();
+            foreach (string fileName in Directory.EnumerateFiles(Config.FortuneDirectory))
+            {
+                if (fileName.Contains("."))
+                {
+                    // probably a .dat file or something similar
+                    continue;
+                }
+
+                string categoryName = Path.GetFileName(fileName);
+                string fortuneText = File.ReadAllText(fileName, StringUtil.Utf8NoBom)
+                    .Replace("\r\n", "\n")
+                    .Replace("\r", "\n");
+                string[] fortunes = fortuneText.Split(
+                    new[] {"\n%\n"},
+                    StringSplitOptions.RemoveEmptyEntries
+                );
+
+                byCategoryBuilder[categoryName] = ImmutableArray.CreateRange(fortunes);
+                allBuilder.AddRange(fortunes);
+            }
+
+            CategoryToFortunes = byCategoryBuilder.ToImmutable();
+            AllFortunes = allBuilder.ToImmutable();
+        }
+
+        protected virtual void HandleFortuneCommand(CommandMatch cmd, IChannelMessageEventArgs args)
+        {
+            string fortuneCategory = ((string)cmd.Arguments[0])?.Trim();
+
+            ImmutableArray<string> fortunes;
+            if (fortuneCategory != null)
+            {
+                if (!CategoryToFortunes.TryGetValue(fortuneCategory, out fortunes))
+                {
+                    ConnectionManager.SendChannelMessage(
+                        args.Channel,
+                        $"{args.SenderNickname}: fortune category \"{fortuneCategory}\" not found"
+                    );
+                    return;
+                }
+            }
+            else
+            {
+                fortunes = AllFortunes;
+            }
+
+            if (fortunes.IsEmpty)
+            {
+                return;
+            }
+
+            int index = RNG.Next(fortunes.Length);
+            ConnectionManager.SendChannelMessage(
+                args.Channel,
+                fortunes[index]
+            );
+            return;
+        }
+    }
+}
