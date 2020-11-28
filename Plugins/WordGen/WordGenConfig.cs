6d8537d490edaff15fd46268f4a212586e472f0e
Ondřej Hošek <ondra.hosek@gmail.com>
2020-11-29 00:18:55 +0100

    add WordGen

---
+++ Plugins/WordGen/WordGenConfig.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace SharpIrcBot.Plugins.WordGen
+{
+    [JsonObject(MemberSerialization.OptOut)]
+    public class WordGenConfig
+    {
+        public List<List<string>> Rings { get; set; }
+        public double? MaxDurationSeconds { get; set; }
+
+        public WordGenConfig(JObject obj)
+        {
+            Rings = new List<List<string>>();
+            MaxDurationSeconds = 5;
+
+            var ser = new JsonSerializer();
+            ser.Populate(obj.CreateReader(), this);
+        }
+    }
+}
---
+++ Plugins/WordGen/WordGenPlugin.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+using SharpIrcBot.Commands;
+using SharpIrcBot.Events.Irc;
+
+namespace SharpIrcBot.Plugins.WordGen
+{
+    public class WordGenPlugin : IPlugin, IReloadableConfiguration
+    {
+        protected WordGenConfig Config { get; set; }
+        protected IConnectionManager ConnectionManager { get; }
+        protected Random RNG { get; set; }
+        protected long LongestWordLength { get; set; }
+
+        public WordGenPlugin(IConnectionManager connMgr, JObject config)
+        {
+            ConnectionManager = connMgr;
+            Config = new WordGenConfig(config);
+            RNG = new Random();
+            LongestWordLength = 0;
+
+            ConnectionManager.CommandManager.RegisterChannelMessageCommandHandler(
+                new Command(
+                    CommandUtil.MakeNames("wordgen"),
+                    CommandUtil.NoOptions,
+                    CommandUtil.MakeArguments(
+                        RestTaker.Instance // substring
+                    ),
+                    CommandUtil.MakeTags("fun"),
+                    forbiddenFlags: MessageFlags.UserBanned
+                ),
+                HandleWordGenCommand
+            );
+        }
+
+        private void HandleWordGenCommand(CommandMatch cmd, IChannelMessageEventArgs msg)
+        {
+            string substr = ((string)cmd.Arguments[0]).Trim();
+
+            if (substr.Length > LongestWordLength)
+            {
+                ConnectionManager.SendChannelMessage(msg.Channel, $"{msg.SenderNickname}: that's longer than the longest word");
+            }
+
+            string foundWord;
+            var wordPieces = new List<string>(Config.Rings.Count);
+            var stopwatch = Stopwatch.StartNew();
+            do
+            {
+                if (Config.MaxDurationSeconds.HasValue && stopwatch.Elapsed > TimeSpan.FromSeconds(Config.MaxDurationSeconds.Value))
+                {
+                    ConnectionManager.SendChannelMessage(msg.Channel, $"{msg.SenderNickname}: sorry, can't find it :-(");
+                    return;
+                }
+
+                wordPieces.Clear();
+                foreach (List<string> ring in Config.Rings)
+                {
+                    int i = RNG.Next(ring.Count);
+                    wordPieces.Add(ring[i]);
+                }
+                foundWord = string.Concat(wordPieces);
+            } while (!foundWord.Contains(substr));
+
+            ConnectionManager.SendChannelMessage(msg.Channel, $"{msg.SenderNickname}: {foundWord}");
+        }
+
+        public virtual void ReloadConfiguration(JObject newConfig)
+        {
+            Config = new WordGenConfig(newConfig);
+            PostConfigReload();
+        }
+
+        protected virtual void PostConfigReload()
+        {
+            // find the longest word
+            int longestLength = 0;
+            foreach (List<string> ring in Config.Rings)
+            {
+                int ringMax = ring.Select(piece => (int?)piece.Length)
+                    .Max() ?? 0;
+                longestLength += ringMax;
+            }
+            LongestWordLength = longestLength;
+        }
+    }
+}
